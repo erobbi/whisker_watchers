c0b7e5b9f4d9668a04241bf5599df0968fdc491c
erobbi <elirobbins0@gmail.com>
2021-09-27 17:06:43 -0400

    built out cat calculator logic

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,4 +4,43 @@ class CatsController < ApplicationController
         cats = Cat.all
         render json: cats, status: :ok
     end
+
+    def bcscalculator
+        bcs = params[:BCS].to_f
+        cw = params[:currentWeight].to_f 
+
+        ratioOverweight = (0.1*bcs - 0.5)
+        percentOverweight = (ratioOverweight*100).to_i;
+        if percentOverweight < 0 
+            warning = true
+        else
+            warning = false
+        end
+        if bsc > 5
+            suggestedIdealWeight = (cw - (cw * ratioOverweight)).round(3)
+        end
+        if bsc < 6
+            suggestedCalories = ((BCS)/2.2)**0.75
+            suggestedCalories = (suggestedCalories * 70).to_i
+        end
+
+        if bsc.between?(6,8)
+            message = "Your cat is #{percentOverweight}% overweight!\nSuggested ideal weight is #{suggestedIdealWeight} pounds"
+        elsif bsc > 8
+            message = "Your cat is obese!\nYour cat is #{percentOverweight}% over ideal weight.\nSuggested ideal weight is #{suggestedIdealWeight} pounds"
+        elsif bsc.between?(4,5)
+            message = "Your cat is a perfect weight!"
+        elsif bsc < 4
+            message = "Your cat is underweight."
+        else
+            message = "Error."
+        end
+
+        bodyFat = params[:currentWeight] * (100)
+
+        messageSuggestion = "no"
+
+        render json: [percentOverWeight: percentOverweight, warning: warning, suggestedCalories: suggestedCalories, message: message], status: :accepted
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  # calculator routes
+  post '/catcalculator', to: 'cats#bcscalculator'
+
 end
