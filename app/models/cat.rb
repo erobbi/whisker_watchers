b7a3d0d0fccfcae93e5423c3eb4c196d30f05a91
erobbi <elirobbins0@gmail.com>
2021-09-30 15:31:40 -0400

    added current_weight method

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,8 @@
 class Cat < ApplicationRecord
   belongs_to :user
   has_many :weights, dependent: :destroy
+
+  def current_weight
+    self.weights.last.weight
+  end
 end
--- app/serializers/cat_serializer.rb
+++ app/serializers/cat_serializer.rb
@@ -1,4 +1,4 @@
 class CatSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :cat_url, :food_per_day, :user_id
+  attributes :id, :name, :age, :cat_url, :food_per_day, :user_id, :current_weight
   has_many :weights
 end
