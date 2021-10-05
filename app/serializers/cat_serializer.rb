32ef418de034880a52a832d0ca33dd2e86d96232
erobbi <elirobbins0@gmail.com>
2021-10-05 16:27:26 -0400

    added date sorting to cat weights

--- app/serializers/cat_serializer.rb
+++ app/serializers/cat_serializer.rb
@@ -1,4 +1,7 @@
 class CatSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :cat_url, :food_per_day, :user_id
-  has_many :weights
+  attributes :id, :name, :age, :cat_url, :food_per_day, :user_id, :weights
+
+  def weights
+    object.weights.sort_by(&:id)
+  end
 end
