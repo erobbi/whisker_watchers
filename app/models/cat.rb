5160cdaf9ed8ddfbd98efe535db9cd85637b0d99
erobbi <elirobbins0@gmail.com>
2021-10-04 21:55:47 -0400

    removed current_weight

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,7 +2,7 @@ class Cat < ApplicationRecord
   belongs_to :user
   has_many :weights, dependent: :destroy
 
-  def current_weight
-    self.weights.last.weight
-  end
+  # def current_weight
+  #   self.weights.last.weight
+  # end
 end
--- app/serializers/cat_serializer.rb
+++ app/serializers/cat_serializer.rb
@@ -1,4 +1,4 @@
 class CatSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :cat_url, :food_per_day, :user_id, :current_weight
+  attributes :id, :name, :age, :cat_url, :food_per_day, :user_id
   has_many :weights
 end
