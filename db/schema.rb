70c8bfc2125e03a08349605957f115a0fa1c1b4c
erobbi <elirobbins0@gmail.com>
2021-10-05 17:21:01 -0400

    changed weights to decial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_231030) do
+ActiveRecord::Schema.define(version: 2021_10_05_210429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_09_22_231030) do
   end
 
   create_table "weights", force: :cascade do |t|
-    t.integer "weight"
+    t.decimal "weight", precision: 3, scale: 1
     t.bigint "cat_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
